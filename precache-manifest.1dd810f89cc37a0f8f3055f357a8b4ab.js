3500069803c1a4142daa685a51640bb5e5386379
greenblue467 <greenblue467@hotmail.com>
2020-02-10 22:42:54 +0800

    Updates

---
+++ precache-manifest.1dd810f89cc37a0f8f3055f357a8b4ab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b01e86ccc3f95602a02f45ceb89a8b",
+    "url": "/p5-shopping/index.html"
+  },
+  {
+    "revision": "fe10f355ea343a0e83df",
+    "url": "/p5-shopping/static/css/main.c24e828d.chunk.css"
+  },
+  {
+    "revision": "a208cf15e34c6af6f246",
+    "url": "/p5-shopping/static/js/2.b79bc274.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/p5-shopping/static/js/2.b79bc274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe10f355ea343a0e83df",
+    "url": "/p5-shopping/static/js/main.078ba613.chunk.js"
+  },
+  {
+    "revision": "c84c9d4de07b6127b2bc",
+    "url": "/p5-shopping/static/js/runtime-main.a75cd4c5.js"
+  },
+  {
+    "revision": "46465899f7e6c126c9e964b88ca8edbc",
+    "url": "/p5-shopping/static/media/p1.46465899.jpg"
+  },
+  {
+    "revision": "bfaf898f33c31c06428104157106bb8b",
+    "url": "/p5-shopping/static/media/p2.bfaf898f.jpg"
+  },
+  {
+    "revision": "4dcb1c4f7c4acd5dd538e9b4cb04f8db",
+    "url": "/p5-shopping/static/media/p3.4dcb1c4f.jpg"
+  },
+  {
+    "revision": "66b080f8435519808f206b85fded1e79",
+    "url": "/p5-shopping/static/media/p4.66b080f8.jpg"
+  },
+  {
+    "revision": "556c31eb5cd87f58e0ad6e56931c735c",
+    "url": "/p5-shopping/static/media/p5.556c31eb.jpg"
+  },
+  {
+    "revision": "057ec44edf1813876247fc9c1f8ccd31",
+    "url": "/p5-shopping/static/media/p6.057ec44e.jpg"
+  },
+  {
+    "revision": "cd2bdb3a52ef909c40aaa3f8804411c6",
+    "url": "/p5-shopping/static/media/p7.cd2bdb3a.jpg"
+  },
+  {
+    "revision": "acc51158152962544e4b4d29b26d3caf",
+    "url": "/p5-shopping/static/media/p8.acc51158.jpg"
+  }
+]);
